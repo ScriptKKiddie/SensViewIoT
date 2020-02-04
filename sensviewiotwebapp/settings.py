7ae543c820c8fd0f1a866a3ac29f142c77cfffeb
Shobhit Sharma <shobhit0812@gmail.com>
2020-02-04 17:24:39 +0530

    SensView IoT - IoT Dashboard v1.0

--- sensviewiotwebapp/settings.py
+++ sensviewiotwebapp/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-                    BASE_DIR + '/static/',
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
